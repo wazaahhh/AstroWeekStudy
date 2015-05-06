5f06b82c4f1c20db3787157eae2dff75ca85106d
wazaahhh <thomas.maillart@gmail.com>
2015-05-06 19:48:38 +0000

    a bench of data backups and exports for manual coding

--- python_code/astroWeekLib.py
+++ python_code/astroWeekLib.py
@@ -4,6 +4,9 @@ import requests
 import re
 import time
 
+username_list = ('jakevdp','ivezic','davidwhogg','profjsb','dhuppenkothen','drphilmarshall','fperez','kbarbary','jonathansick','Waelthus','nell-byler','jradavenport','gully','bareid','ogtelford','sofiatti','karenyyng','adrn','kapadia','rbiswas4','rhiannonlynne','lvrzhn','jmankin','wilmatrick','RuthAngus','nhuntwalker','ryanmaas','patti','yoachim','yalsayyad')
+
+
 github_credentials = json.loads(open("/home/ubuntu/.github").read())
 github_token = github_credentials['access_token']
 
@@ -42,13 +45,35 @@ def getPagedRequest(url):
         time.sleep(0.25)
     return results
 
+def retrieveUserInfo(username):
+    user_api_url = 'users/%s'%username
+    url = api_url + user_api_url + "?access_token=%s"%github_token
+    print url
+    user_info = getPagedRequest(url)[0]
+    
+    time.sleep(1)
+    #try:
+    #events_api_url = user_info['events_url'][:-10] + "/public"
+    #print events_api_url
+    #user_events = getPagedRequest(events_api_url)
+    return {"info" : user_info}
+
+
+def retrieveAllUserInfo(username_list):
+
+    dic = {}
+
+    for u in username_list:
+        dic[u] = retrieveUserInfo(u)
+
+    return dic
 
 if __name__ == '__main__':
-    user_events = 'users/%s/received_events/public'%'jakevdp'
-
-    url = api_url + user_events + "?access_token=%s"%github_token
-    print url
-    test = getPagedRequest(url)
+    print "blah"
+    #user_events = 'users/%s'%'jakevdp'
+    #url = api_url + user_events + "?access_token=%s"%github_token
+    #print url
+    #test = getPagedRequest(url)
     
     #for i in range(len(test)):
     #    print test[i]['type'],test[i]['created_at'],test[i]['repo']['name']
